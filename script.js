6ec3f151cd68412e7e073b889639a15b848a2aa6
anandvc <anandalpz3@gmail.com>
2021-11-26 15:17:06 +0530

    calculator commit1

---
+++ script.js
@@ -0,0 +1,7 @@
+var displayValue=num=>result.value+=num
+
+var clearBox=()=>result.value=""
+
+var evaluateExp=()=>result.value=eval(result.value)
+
+var backSpace=()=>result.value=result.value.slice(0,-1)
